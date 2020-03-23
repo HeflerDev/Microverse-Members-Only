a1614da2d010951522869f7553e92324a4767105
Kasuhira <hheflerdevelopment@gmail.com>
2020-03-23 17:03:32 -0300

    Fixed Cookies Issue

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,26 @@
 module SessionsHelper
   def log_in(user)
-    session[:user_id] = user.id
+    remember_digest = User.digest(User.new_token)
+    user.update_attribute(:remember_digest, remember_digest)
+    cookies.permanent[:remember_token] = remember_digest
+    current_user = user
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(remember_digest: cookies.permanent[:remember_token])
   end
 
   def logged_in?
     !current_user.nil?
   end
 
+  def forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget
     session.delete(:user_id)
     @current_user = nil
   end
